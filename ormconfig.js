103677743545fb8aa1eb2f8a2c51b9435460a7a5
Marja de Sordi <marjadesordi@gmail.com>
2021-12-14 11:26:29 -0300

    correção das variáveis

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports =  {
   "type": "mysql",
-  "host": process.env.host,
-  "port": process.env.port,
+  "host": process.env.localhost,
+  "port": process.env.PORT,
   "username": process.env.user,
   "password": process.env.password,
   "database": process.env.database,
@@ -16,8 +16,9 @@ module.exports =  {
   "cli": {
     "migrationsDir": "src/database/migrations"
   },
-  "insecureAuth": true, 
-  "synchronize": true,
+  "insecureAuth": true,
+ " multipleStatements": true 
+
 }
 
 console.log('aqui2');
