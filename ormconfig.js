38033929084713b9fe44800ad1018ced4530f225
Marja de Sordi <marjadesordi@gmail.com>
2021-12-14 11:38:56 -0300

    timeout

--- ormconfig.js
+++ ormconfig.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 module.exports =  {
   "type": "mysql",
   "host": process.env.localhost,
-  "port": process.env.PORT,
+  "port": process.env.PORT || 3000,
   "username": process.env.user,
   "password": process.env.password,
   "database": process.env.database,
+  "timeout"      : 60 * 60 * 1000,
   "entities": [
      "dist/app/models/*.js"
   ],
