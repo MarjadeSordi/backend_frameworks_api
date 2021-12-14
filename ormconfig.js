db4bff900470bc997293174ee2d91d8e499ac9d2
Marja de Sordi <marjadesordi@gmail.com>
2021-12-14 13:24:02 -0300

    port

--- ormconfig.js
+++ ormconfig.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports =  {
   "type": "mysql",
   "host": process.env.localhost,
+  "port": process.env.PORT,
   "username": process.env.user,
   "password": process.env.password,
   "database": process.env.database,
